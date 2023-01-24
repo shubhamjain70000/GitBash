a0535186860f269044f8311ba62ada04702c6810
Shubham <shubhamjain02081996@gmail.com>
2023-01-24 12:49:26 +0530

    demo1,2 test

---
+++ src/test/java/demoPractice/Demo1_Test.java
@@ -0,0 +1,25 @@
+package demoPractice;
+
+import org.testng.annotations.Test;
+
+public class Demo1_Test {
+	
+	@Test
+	public void create()
+	{
+		System.out.println("created");
+	}
+	
+	@Test
+	public void update()
+	{
+		System.out.println("updated");
+	}
+	
+	@Test
+	public void delete()
+	{
+		System.out.println("deleted");
+	}
+
+}
---
+++ src/test/java/demoPractice/Demo2_Test.java
@@ -0,0 +1,25 @@
+package demoPractice;
+
+import org.testng.annotations.Test;
+
+public class Demo2_Test {
+	
+	@Test
+	public void modify()
+	{
+		System.out.println("--modified--");
+	}
+	
+	@Test
+	public void insert()
+	{
+		System.out.println("--inserted--");
+	}
+	
+	@Test
+	public void repeat()
+	{
+		System.out.println("--repeated--");
+	}
+
+}
